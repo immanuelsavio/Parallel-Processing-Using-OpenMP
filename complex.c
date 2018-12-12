b2cc3bf7bbcc20a64f3cb9a42ffe6967f7948641
_Algorithm_Kid98 <30970161+immanuelsavio@users.noreply.github.com>
2018-12-12 21:06:55 +0530

    completed and ready to deply

--- complex.c
+++ complex.c
@@ -8,3 +8,11 @@ void _complex_from_array(float *srcRe, float *srcIm, complex_t *dest, int length
 		dest[n].im = srcIm[n];
 	}
 }
+void _complex_from_real(float *src, complex_t *dest, int length) {
+	int n;
+
+	for (n = 0; n < length; n ++) {
+		dest[n].re = src[n];
+		dest[n].im = 0;
+	}
+}

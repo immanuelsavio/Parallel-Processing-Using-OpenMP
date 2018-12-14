f5c50c8718e1647b33b4fd404293869648b5f233
_Algorithm_Kid98 <30970161+immanuelsavio@users.noreply.github.com>
2018-12-14 19:15:17 +0530

    Project done!!!

---
+++ radix4.h
@@ -0,0 +1,9 @@
+#ifndef _RADIX4_H
+#define _RADIX2_H
+
+#include "complex.h"
+
+void radix4(complex_t *data, int length);
+void radix4reorder(complex_t *dataIn, complex_t *dataOut, int length);
+
+#endif /* _RADIX4_H */

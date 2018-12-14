3af71e81fe45ddd931739fdb8a7580955f9c964d
_Algorithm_Kid98 <30970161+immanuelsavio@users.noreply.github.com>
2018-12-14 19:14:24 +0530

    done!!!

---
+++ radix2.h
@@ -0,0 +1,9 @@
+#ifndef _RADIX2_H
+#define _RADIX2_H
+
+#include "complex.h"
+
+void radix2(complex_t *data, int length);
+void radix2reorder(complex_t *dataIn, complex_t *dataOut, int length);
+
+#endif /* _RADIX2_H */
